cc68ee674a219b6ffcbd874ae7e8a47a457cde77
Faith-Watiri <watiri216@gmail.com>
2024-10-02 03:39:37 +0000

    comit

--- 0x00-ES6_basic/100-main.js
+++ 0x00-ES6_basic/100-main.js
@@ -1,17 +1,9 @@
-import createIteratorObject from "./100-createIteratorObject.js";
-
-import createEmployeesObject from './11-createEmployeesObject.js';
-import createReportObject from './12-createReportObject.js';
-
-const employees = {
-    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-    ...createEmployeesObject('marketing', ['Sylvie'])
-};
-
-const report = createReportObject(employees);
-
-const reportWithIterator = createIteratorObject(report);
-
-for (const item of reportWithIterator) {
-    console.log(item);
+export default function iterateThroughObject(reportWithIterator) {
+  let result = '';
+  for (const employee of reportWithIterator) {
+    result += `${employee} | `;
+  }
+  // Remove the trailing ' | ' if there is any
+  return result.slice(0, -3);
 }
+
