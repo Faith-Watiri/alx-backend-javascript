e0b68ef6b224536edc0cddaf17f0664f2bcecf18
Faith-Watiri <watiri216@gmail.com>
2024-10-02 02:26:28 +0000

    commit

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,6 @@
+export default function getSumOfHoods(initialValue){
+return initialValue + 2;
+}
+console.log(getSumOfHoods(34));
+console.log(getSumOfHoods(34, 3));
+console.log(getSumOfHoods(34, 3, 4));
